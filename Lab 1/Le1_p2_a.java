a9229f5deb86aa8737f9a6c7d84f0403ef1925c6
areeb211b181 <126743518+areeb211b181@users.noreply.github.com>
2023-05-17 18:30:37 +0530

    LAB 1

---
+++ Lab 1/Le1_p2_a.java
@@ -0,0 +1,23 @@
+class Main  
+{    
+public static void main(String args[])   
+{    
+int i, j, n = 4;       
+for (i=0; i<n; i++)   
+{  
+for (j=n-i; j>1; j--)   
+{  
+
+System.out.print(" ");   
+}   
+
+for (j=0; j<=i; j++ )   
+{   
+   
+System.out.print("* ");   
+}   
+ 
+System.out.println();   
+}   
+}   
+}  
---
+++ Lab 1/Le1_p3.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        int v=Integer.parseInt(args[0]);
+        
+       for(int i=1;i<=10;i++)
+       {
+           
+           System.out.println(v+"*"+" "+i+"="+v*i+"");
+       }
+    }
+}
